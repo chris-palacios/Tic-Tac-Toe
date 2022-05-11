f144235379beab232f67364073c5efe0c5e3e0b4
Christian Palacios <79600301+chris-palacios@users.noreply.github.com>
2022-05-10 17:09:14 -0700

    UPDATE MAIN.CPP
    Added 2 player mode.

--- Main.cpp
+++ Main.cpp
@@ -5,7 +5,7 @@ void placePiece(char board[][3], int location, char playerChar){
   while(location > 9 || location < 1){
     cout << "Please select a number that is present on the board between 1 and 9" << endl;
     cin >> location;
-  }
+  }                                     // NOTE: NEED TO IMPLIMENT LOOP FOR PLACING A PIECE THAT ALREADY HAS SOMETHING IN IT
   if(location < 4){
     board[0][location-1] = playerChar; // If location is less than 4 -1 to set in array
   }
@@ -25,22 +25,19 @@ bool checkDiagonalRight(char board[][3]){
     	cout << endl << board[0][2] << " is the winner right corner diagonal"; 
         return 1;
     }	    
-    
-    cout << "Nothing happened";
-    
+
     return 0;
 
 }
-bool checkDiagonalLeft(char board[][3]){
+bool checkDiagonalLeft(char board[][3]){              
     if(board[0][0] == '\0' || board[2][2] == '\0'){
       return 0;
 }
     if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
     	cout << endl << board[2][2] << " is the winner left corner diagonal"; 
-		
+
 		return 1; 	
     }
-    cout << "Nothing happened";
     
     return 0;
 }
@@ -54,6 +51,7 @@ bool checkVertical(char board[][3]){
             return 1;
         }
     }
+    return 0;
 }
 bool checkHorizontal(char board[][3]){
 	for (int i = 0; i < 3; i++){
@@ -61,20 +59,25 @@ bool checkHorizontal(char board[][3]){
 	    continue;
 	  }
 		if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
-        	cout << endl << board[i][3] << " is the winner Horizontal win row " << 1 + i << endl;
+        	cout << endl << board[i][2] << " is the winner Horizontal win row " << 1 + i << endl;
             return 1;
         }
     }
+    return 0;
 }
-void checkBoard(char board[][3]){
-  checkHorizontal(board);
-  checkVertical(board);
-  checkDiagonalLeft(board);
-  checkDiagonalRight(board);
+bool checkBoard(char board[][3]){
+  if(checkHorizontal(board)||checkVertical(board)||checkDiagonalLeft(board)
+  ||checkDiagonalRight(board)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 }
 
 void printBoard(char board[][3]){
   int counter = 1; // counter for numbering the play table
+  cout << endl;
 for(int i = 0; i < 3; i++){
 	for(int j = 0; j < 3; j++){
     	if(board[i][j] != '\0'){ 
@@ -94,11 +97,53 @@ for(int i = 0; i < 3; i++){
 }
 
 int main() {
-char board[3][3] = {{'\0','\0','\0'},
-				          	{'\0','\0','\0'},
-                    {'\0','\0','\0'}};
-printBoard(board);
-// placePiece(board, selection ,playerChar)
-checkBoard(board);
-          
-}
+  int playerSelection = 0;
+  char board[3][3] = {{'\0','\0','\0'},
+	  			      {'\0','\0','\0'},
+                      {'\0','\0','\0'}};
+
+  int numOfPlayers = 0;
+  cout <<"Welcome to the game of tic-tac-toe place 3 pieces in a row in order to win."<< endl;
+  while(numOfPlayers > 2 || numOfPlayers < 1){
+    cout << "Are there 1 or 2 players?" << endl;
+    cin >> numOfPlayers;
+    printBoard(board);
+  }
+  switch (numOfPlayers) {
+  case 1:               // NEED TO IMPLIMENT SOME TIME OF CPU OPPONENT
+    cout << endl << "Single player" << endl;
+    while(!checkBoard(board)){
+      cout<<"Where would you like to place a piece?"<<endl;
+      cin >> playerSelection;
+      placePiece(board, playerSelection, 'X');
+      printBoard(board);
+      cout << endl;
+    }
+    break;
+  case 2:
+    cout << endl << "Two players" << endl;
+    char player1 = 'X'; // player1 assigned x
+    char player2 = 'O'; // player2 assigned o 
+    while(!checkBoard(board)){
+      cout<<"Player 1: Where would you like to place a piece?"<<endl;
+      cin >> playerSelection;
+      placePiece(board, playerSelection, player1);
+      printBoard(board);
+      cout << endl;
+      if(!checkBoard(board)){
+        cout<<"Player 2: Where would you like to place a piece?"<<endl;
+        cin >> playerSelection;
+        placePiece(board, playerSelection, player2);
+        printBoard(board);
+        cout << endl;
+      }
+      else{
+          break;
+      }
+    }
+    break;
+  }
+
+
+
+
